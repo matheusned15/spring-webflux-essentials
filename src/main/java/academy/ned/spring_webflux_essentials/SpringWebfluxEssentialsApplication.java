fc8a748ab17f13eb52cfc1a664ec417553c6f70d
matheusned15 <matheusned15@gmail.com>
2024-05-31 15:00:49 -0300

    Global Exception Handler

--- src/main/java/academy/ned/spring_webflux_essentials/SpringWebfluxEssentialsApplication.java
+++ src/main/java/academy/ned/spring_webflux_essentials/SpringWebfluxEssentialsApplication.java
@@ -1,7 +1,12 @@
 package academy.ned.spring_webflux_essentials;
 
+import academy.ned.spring_webflux_essentials.exception.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
@@ -13,6 +18,11 @@ public class SpringWebfluxEssentialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
+	}
+
+	@Bean
+	public GlobalExceptionHandler globalExceptionHandler(ErrorAttributes errorAttributes, WebProperties webProperties, ApplicationContext applicationContext) {
+		return new GlobalExceptionHandler(errorAttributes, webProperties, applicationContext);
 	}
 
 }
---
+++ src/main/java/academy/ned/spring_webflux_essentials/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package academy.ned.spring_webflux_essentials.exception;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Component
+@Order(-2)
+public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalExceptionHandler(ErrorAttributes errorAttributes, WebProperties webProperties, ApplicationContext applicationContext) {
+        super(errorAttributes, webProperties.getResources(), applicationContext);
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        Map<String, Object> errorPropertiesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        ServerResponse.BodyBuilder responseBuilder = ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        return responseBuilder.contentType(MediaType.APPLICATION_JSON).bodyValue(errorPropertiesMap);
+    }
+}
+
