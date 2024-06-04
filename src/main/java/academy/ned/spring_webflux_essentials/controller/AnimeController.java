22c164f5b214037c18b4c25dfabe9d4d71a8330e
matheusned15 <matheusned15@gmail.com>
2024-06-04 17:37:07 -0300

    Reactive POST and PUT

--- src/main/java/academy/ned/spring_webflux_essentials/controller/AnimeController.java
+++ src/main/java/academy/ned/spring_webflux_essentials/controller/AnimeController.java
@@ -3,13 +3,11 @@ package academy.ned.spring_webflux_essentials.controller;
 import academy.ned.spring_webflux_essentials.domain.Anime;
 import academy.ned.spring_webflux_essentials.repository.AnimeRepository;
 import academy.ned.spring_webflux_essentials.service.AnimeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -29,5 +27,17 @@ public class AnimeController {
     @GetMapping(path = "{id}")
     public Mono<Anime> findById(@PathVariable int id){
         return animeService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Anime> save(@Valid @RequestBody Anime anime){
+        return animeService.save(anime);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> update(@Valid @RequestBody Anime anime){
+        return animeService.update(anime);
     }
 }
