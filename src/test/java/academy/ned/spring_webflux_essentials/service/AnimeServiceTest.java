3bb8c58780a59b99ad79db0ef603721b959746c3
matheusned15 <matheusned15@gmail.com>
2024-06-07 14:20:25 -0300

    Unit Test Service pt 02

--- src/test/java/academy/ned/spring_webflux_essentials/service/AnimeServiceTest.java
+++ src/test/java/academy/ned/spring_webflux_essentials/service/AnimeServiceTest.java
@@ -3,16 +3,20 @@ package academy.ned.spring_webflux_essentials.service;
 import academy.ned.spring_webflux_essentials.domain.Anime;
 import academy.ned.spring_webflux_essentials.repository.AnimeRepository;
 import academy.ned.spring_webflux_essentials.util.AnimeCreator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +30,7 @@ class AnimeServiceTest {
     private AnimeService animeService;
 
     @Mock
-    private AnimeRepository animeRepository;
+    private AnimeRepository animeRepositoryMock;
 
     private final Anime anime = AnimeCreator.createAnimeValidAnime();
 
@@ -49,6 +53,45 @@ class AnimeServiceTest {
     @BeforeAll
     public static void blockHoundSetup(){
         BlockHound.install();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        BDDMockito.when(animeRepositoryMock.findAll())
+                .thenReturn(Flux.just(anime));
+
+        BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.just(anime));
+    }
+
+    @Test
+    @DisplayName("findall returns a flux of anime")
+    public void findAll_ReturnsFluxOfAnime_WhenSuccessful(){
+        StepVerifier.create(animeService.findAll())
+                .expectSubscription()
+                .expectNext(anime)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("findById returns Mono with anime when it exists")
+    public void findById_ReturnsMonoAnime_WhenSuccessful(){
+        StepVerifier.create(animeService.findById(1))
+                .expectSubscription()
+                .expectNext(anime)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("findById returns Mono error when anime does not exists")
+    public void findById_ReturnsMonoError_WhenEmptyMonoIsReturned(){
+        BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.empty());
+
+        StepVerifier.create(animeService.findById(1))
+                .expectSubscription()
+                .expectError(ResponseStatusException.class)
+                .verify();
     }
 
 
